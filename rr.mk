22ee583da932321125cfc55b9f14134cae2ff97e
alnasnasu <36354143+alnasnasu@users.noreply.github.com>
2018-04-14 13:03:45 +0530

    Update rr.mk

--- rr.mk
+++ rr.mk
@@ -4,7 +4,7 @@
 PRODUCT_RELEASE_NAME := A311
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Micromax/A311/device_A311.mk)
